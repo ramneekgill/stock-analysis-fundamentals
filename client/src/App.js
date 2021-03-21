284d16f5c33d5d0a40a2f6490b5ad3d111924330
Ramneek Gill <ramneekg25@gmail.com>
2021-03-21 15:08:50 -0600

    Add routing/redirects and refactor code out of main app page into jsx components

--- client/src/App.js
+++ client/src/App.js
@@ -1,150 +1,22 @@
-import React, { useEffect, useState, useRef } from "react";
-import "./App.css";
-import AutoComplete from './Components/getAutocomplete'
-import FinancialDataService from './Services/financialdata.service'
-import Axios from "axios";
-import Cookies from 'js-cookie';
-var dict = require('./nyse-listed_json.json');
-
-
-
-
-
-const Auto = () => {
-  const [display, setDisplay] = useState(false);
-  const [options, setOptions] = useState([]);
-  const [search, setSearch] = useState("");
-  const wrapperRef = useRef(null);
-
-  useEffect(() => {
-    // const pokemon = [];
-    // const promises = new Array(20)
-    //   .fill()
-    //   .map((v, i) => fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
-    // Promise.all(promises).then(pokemonArr => {
-    //   return pokemonArr.map(value =>
-    //     value
-    //       .json()
-    //       .then(({ name, sprites: { front_default: sprite } }) =>
-    //         pokemon.push({ name, sprite })
-    //       )
-    //   );
-    // });
-
-    // console.log(pokemon);
-    
-    setOptions(dict);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      window.removeEventListener("mousedown", handleClickOutside);
-    };
-  });
-
-  const handleClickOutside = event => {
-    const { current: wrap } = wrapperRef;
-    if (wrap && !wrap.contains(event.target)) {
-      setDisplay(false);
-    }
-  };
-
-  const updatePokeDex = poke => {
-    setSearch(poke);
-    setDisplay(false);
-  };
-
-  const postCompany = () => {
-    const csrftoken = Cookies.get('csrftoken');
-    console.log(csrftoken);
-    // Axios.post(`http://127.0.0.1:8000/financialdata/`, {
-    //         'companyName': 'something'
-    //     },
-    //     {
-    //         headers: {
-    //         'Content-Type': 'application/json',
-    //         'X-CSRFToken': csrftoken
-    //         },
-    //         withCredentials: true,
-    //     }
-    // )
-    // .then(res => console.log(res))
-    // .catch(error => console.error(error))
-
-    FinancialDataService.create({'Symbol': search})
-    .then(res => console.log(res.data))
-    .catch(error => console.error(error))
-    }
-
-
-
-  return (
-    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
-      <input
-        id="auto"
-        placeholder="Type to search"
-        value={search}
-        onChange={event => {
-          if(AutoComplete.get(event.target.value).length > 0){
-            setDisplay(true);
-          }
-          else{
-            setDisplay(false);
-          }
-          setSearch(event.target.value);
-          setOptions(AutoComplete.get(event.target.value))
-        }}
-        autoComplete="off"
-      />
-      {display && (
-        <div className="autoContainer">
-          <div className="labelbox"
-          tabIndex="0">
-            <span className="label">Name</span>
-            <span className="label">Symbol</span>
-          </div>
-          {options
-            .slice(0,5)
-            .filter(value => {
-              return value["Symbol"].indexOf("$") === -1
-            })
-            .map((value, i) => {
-              return (
-                <div
-                  onClick={() => updatePokeDex(value["Symbol"])}
-                  className="option"
-                  key={i}
-                  tabIndex="0"
-                >
-                  <span className="company">{value["Company Name"]}</span>
-                  <span className="symbol">{value["Symbol"]}</span>
-                </div>
-              );
-            })
-            }
-        </div>
-      )}
-      <input className="button"
-      type="submit" 
-      value="Generate Report" 
-      onClick={() => {postCompany()}}
-      />
-    </div>
-    
-  );
-};
+import { Route, Redirect, Switch } from "react-router-dom";
+import SearchPage from './Components/SearchPage'
+import CompanyAnalysis from './Components/CompanyAnalysis'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Search Company</h1>
-      <div className="logo"></div>
-      <div className="auto-container">
-        <Auto />
-      </div>
-    </div>
+    <main className="container">
+      <Switch>
+        <Route exact path="/about">
+          <CompanyAnalysis />
+        </Route>
+        <Route exact path="/search">
+          <SearchPage />
+        </Route>
+        <Redirect exact from="/" to="/search" />
+      </Switch>
+    </main>
   );
 }
 
 export default App;
+
---
+++ client/src/Components/SearchPage.jsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useState, useRef } from "react";
+import "../App.css";
+import AutoComplete from './getAutocomplete'
+import FinancialDataService from '../Services/financialdata.service'
+import Cookies from 'js-cookie';
+import {
+  BrowserRouter as Link
+} from "react-router-dom";
+
+
+
+var dict = require('../nyse-listed_json.json');
+
+
+const Auto = () => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    setOptions(dict);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = event => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const updatePokeDex = poke => {
+    setSearch(poke);
+    setDisplay(false);
+  };
+
+  const postCompany = () => {
+    const csrftoken = Cookies.get('csrftoken');
+    var responseData = "test";
+    console.log(csrftoken);
+    // Axios.post(`http://127.0.0.1:8000/financialdata/`, {
+    //         'companyName': 'something'
+    //     },
+    //     {
+    //         headers: {
+    //         'Content-Type': 'application/json',
+    //         'X-CSRFToken': csrftoken
+    //         },
+    //         withCredentials: true,
+    //     }
+    // )
+    // .then(res => <CompanyAnalysis companyData=res />)
+    // .catch(error => console.error(error))
+
+    FinancialDataService.create({'Symbol': search})
+    .then(res => console.log(res))
+
+    }
+
+
+
+  return (
+    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
+      <input
+        id="auto"
+        placeholder="Type to search"
+        value={search}
+        onChange={event => {
+          if(AutoComplete.get(event.target.value).length > 0){
+            setDisplay(true);
+          }
+          else{
+            setDisplay(false);
+          }
+          setSearch(event.target.value);
+          setOptions(AutoComplete.get(event.target.value))
+        }}
+        autoComplete="off"
+      />
+      {display && (
+        <div className="autoContainer">
+          <div className="labelbox"
+          tabIndex="0">
+            <span className="label">Name</span>
+            <span className="label">Symbol</span>
+          </div>
+          {options
+            .slice(0,5)
+            .filter(value => {
+              return value["Symbol"].indexOf("$") === -1
+            })
+            .map((value, i) => {
+              return (
+                <div
+                  onClick={() => updatePokeDex(value["Symbol"])}
+                  className="option"
+                  key={i}
+                  tabIndex="0"
+                >
+                  <span className="company">{value["Company Name"]}</span>
+                  <span className="symbol">{value["Symbol"]}</span>
+                </div>
+              );
+            })
+            }
+        </div>
+      )}
+      <input className="button"
+      type="submit" 
+      value="Generate Report" 
+      onClick={() => {postCompany()}}
+      />
+    </div>
+
+    
+  );
+};
+
+export default function SearchPage(props) {
+    return (
+        <div className="App">
+            <h1>Search Company</h1>
+            <div className="logo"></div>
+            <div className="auto-container">
+                <Auto />
+            </div>
+        </div>
+    );
+  }
