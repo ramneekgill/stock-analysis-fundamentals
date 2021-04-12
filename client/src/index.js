d7eb684caf330ba19c196a6972762c8a9c72047d
Ramneek Gill <ramneekg25@gmail.com>
2021-04-11 20:52:14 -0600

    added http to https redirect

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import HttpsRedirect from 'react-https-redirect';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>,
+  <HttpsRedirect>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </HttpsRedirect>,
   document.getElementById('root')
 );
 
