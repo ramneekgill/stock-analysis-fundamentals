077d4acff4384f74a7dafcfaebe405b162b1735f
Ramneek Gill <ramneekg25@gmail.com>
2021-04-11 21:15:33 -0600

    update url on axios for development server domain

--- client/src/Services/financialdata.service.js
+++ client/src/Services/financialdata.service.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
-axios.get('https://stock-analysis-fundamentals.herokuapp.com/financialdata/', {withCredentials: true}).then((res) => {Cookies.set('csrftoken', res.data.csrfToken)});
+axios.get('http://127.0.0.1:8000', {withCredentials: true}).then((res) => {Cookies.set('csrftoken', res.data.csrfToken)});
 
 class FinancialDataService {
   create(data) {
     var csrfToken = Cookies.get('csrftoken');
     const instance = axios.create({
-      baseURL: "https://stock-analysis-fundamentals.herokuapp.com",
+      baseURL: "http://127.0.0.1:8000",
       headers: {
         "Content-type": "application/json",
         'X-CSRFToken': csrfToken
